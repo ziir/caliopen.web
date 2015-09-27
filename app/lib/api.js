895b7fec0dc38aa0099651fffc815b4b7bb9b19e
Tim PILLARD <tpillardpro@gmail.com>
2015-09-27 17:05:55 +0200

    Correctly use http.request
    Improve Auth and api.query method

--- app/lib/api.js
+++ app/lib/api.js
@@ -6,13 +6,53 @@ var debug = require('debug')('caliopen.web:app:api-query');
 function query(params) {
   var options = ObjectAssign({}, this.defaults || {}, params);
 
-  debug('\n','Outgoing API query:', '\n', options);
+  if (options.body) {
+    options.body = JSON.stringify(options.body);
+    options.headers['Content-Length'] = Buffer.byteLength(options.body);
+  }
 
-  return http.request(options, function queryResponseCallback(response) {
-    debug('\n','API query response:', '\n', response);
+  debug('\n','Preparing API query:', '\n', options);
+
+  var req = http.request(options, function queryResponseCallback(res) {
+    debug(
+      '\n',
+      'API query response:',
+      '\n',
+      res.statusCode,
+      res.statusMessage,
+      res.headers
+    );
+
+    var data;
+
+    res.on('data', function dataCallback(data) {
+      data = data;
+    });
+
+    res.on('end', function endCallback() {
+      if (res && res.statusCode > 200 && res.statusCode < 300) {
+        !options.success || options.success(data);
+      } else {
+        !options.error || options.error(
+          new Error(
+            'API Query Error ' + res.statusCode + ' : ' + res.statusMessage
+          )
+        );
+      }
+    });
+
   }).on('response', options.response)
-    .on('success', options.success)
     .on('error', options.error);
+
+  if (options.body) {
+    req.write(options.body);
+  }
+
+  debug('\n','Outgoing API query:', '\n', req);
+
+  req.end();
+
+  return req;
 }
 
 module.exports = {
