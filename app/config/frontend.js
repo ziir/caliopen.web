f644cccce1198a741b169d293fdc0d60f94e0965
David Epely <esion99@gmail.com>
2015-10-12 17:20:07 +0200

    fix(middleware): brand logo is missing
    detect injected number by ember app in logo filename

--- app/config/frontend.js
+++ app/config/frontend.js
@@ -3,30 +3,51 @@ var fs = require('fs');
 
 var debug = require('debug')('caliopen.web:app');
 
+function readdirAndFilter(path, filterCallback) {
+  var files = [];
+
+  try {
+    files = fs.readdirSync(path);
+  } catch(err) {
+    debug(
+      'Frontend directory is not available. Style Sheets and brand logo will not be loaded.'
+    );
+    debug(err);
+  }
+
+  var filenames = [];
+  files.forEach(function(filename) {
+    if(filterCallback(filename)) {
+      filenames.push(filename);
+    }
+  });
+
+  return filenames;
+}
 
 module.exports = function frontendConfig(config) {
   var staticFrontendPath = path.resolve(
     __dirname, '..', '..', config.frontend.path, 'dist', 'assets'
   );
-  var files = [];
 
-  try {
-    files = fs.readdirSync(staticFrontendPath);
-  } catch(err) {
-    debug(
-      'Frontend directory is not available. Style Sheets will not be loaded.'
-    );
-    debug(err);
-  }
-
-  files.forEach(function(filename) {
-    if (
+  //css
+  readdirAndFilter(staticFrontendPath, function(filename) {
+    return (
       filename.indexOf('caliopen-frontend') === 0 &&
-      filename.indexOf('.css')!== -1 ||
+      filename.indexOf('.css') !== -1 ||
       filename.indexOf('vendor') === 0 &&
-      filename.indexOf('.css')!== -1
-    ) {
-      config.frontend.cssFiles.push(filename);
-    }
+      filename.indexOf('.css') !== -1
+    );
+  }).forEach(function(filename) {
+    config.frontend.cssFiles.push('assets/' + filename);
   });
+
+  //brand
+  var brandImage = readdirAndFilter(path.resolve(staticFrontendPath, 'images'), function(filename) {
+    return filename.indexOf('brand') === 0 && filename.indexOf('.png') !== -1;
+  }).shift();
+
+  if (brandImage) {
+    config.frontend.brandImage = 'assets/images/' + brandImage;
+  }
 };
--- app/middlewares/frontend-renderer.js
+++ app/middlewares/frontend-renderer.js
@@ -3,6 +3,9 @@ module.exports = function frontendRendererMiddleware(req, res, next) {
   res.locals.head = {
     cssFiles: req.config.frontend.cssFiles
   };
+  res.locals.body = {
+    brandImage: req.config.frontend.brandImage
+  };
 
   return next();
 };
