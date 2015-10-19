72d5fe2e90317c84b98440bffbd33586c74bdec9
David Epely <esion99@gmail.com>
2015-10-19 14:05:44 +0200

    feat(api): add simple proxy to api
    inject Authorisation header

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,25 @@
 'use strict';
 
 var router = require('express').Router();
+var debug = require('debug')('caliopen.web:api');
+var path = require('path');
 
-router.get('/', function () {});
+// For options, see:
+// https://github.com/nodejitsu/node-http-proxy
+var proxy = require('http-proxy').createProxyServer({
+  xfwd: true,
+});
+
+proxy.on('error', function errorCallback(err, req) {
+  debug(err, req.url);
+});
+router.use(function proxyRouting(req, res) {
+  //TODO refactor in Auth library may be or ...
+  req.headers.Authorization = 'Bearer ' + new Buffer(req.user.user_id + ':' + req.user.tokens.access_token).toString('base64');
+  // include root path in proxied request
+  req.url = path.join(req.config.api.prefix, req.url);
+  var options = { target: 'http://' + req.config.api.hostname + ':' + req.config.api.port };
+  proxy.web(req, res, options);
+});
 
 module.exports = router;
